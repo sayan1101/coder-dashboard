ef81d0454f72793bef6f5fee0d921bda48b442bb
unknown <f20191088@goa.bits-pilani.ac.in>
2024-07-09 16:47:11 +0530

    Replace bcrypt with bcryptjs to fix deployment issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const authController = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 
