f7ab6271116f81872c411d88ffde6ef3192e643f
Dhruv Agrawal <80682622+hirsuitedevil@users.noreply.github.com>
2024-07-09 15:27:12 +0530

    cleaned index.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   platforms: {
-    leetcode: { type: String, default: '' },
-    codeforces: { type: String, default: '' },
-    codechef: { type: String, default: '' },
-    hackerrank: { type: String, default: '' },
+    leetcode: { type: String, default: "" },
+    codeforces: { type: String, default: "" },
+    codechef: { type: String, default: "" },
+    hackerrank: { type: String, default: "" },
+  },
+  profileImg: {
+    type: String,
+    default: "",
   },
 });
 
