c03ab13ce259787bbd8d34eb6f18b29e07d8e996
unknown <f20191088@goa.bits-pilani.ac.in>
2024-07-10 11:45:35 +0530

    check username exists endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -178,4 +178,17 @@ authController.put("/updateProfile", verifyToken, async (req, res) => {
   }
 });
 
+authController.post("/check-username", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (user) {
+      return res.status(200).json({ exists: true });
+    } else {
+      return res.status(200).json({ exists: false });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = authController;
