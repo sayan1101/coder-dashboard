dda889ea2ab0f53655f491a824392c7d13e48cae
unknown <f20191088@goa.bits-pilani.ac.in>
2024-07-10 11:48:11 +0530

    added some things

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   platforms: {
@@ -16,7 +17,9 @@ const UserSchema = new mongoose.Schema({
   },
   bio: { type: String, default: "" },
   organization: { type: String, default: "" },
-  country: { type: String, default: "" }
+  country: { type: String, default: "" },
+  followers: { type: Number, default: 0 },
+  following: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('User', UserSchema);
